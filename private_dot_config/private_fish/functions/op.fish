c0a4e83ca2a8b328037652eaa1831b967631ff3e
Matt Williams <m@technovangelist.com>
2022-01-08 13:50:10 -0800

    Added gitconfig

---
+++ private_dot_config/private_fish/functions/op.fish
@@ -0,0 +1,11 @@
+# function op
+#     if set -q OP_SESSION_my
+#         echo "can op $argv"
+#         /usr/local/bin/op $argv
+#     else
+#         echo "cannot op"
+#         eval (/usr/local/bin/op signin my)
+#         echo "now can op $argv"
+#     end
+
+# end
